f94ed9306273ee0897d9403c5c9a16a6224d3407
Akari Katsuma <katainu0103@gmail.com>
2021-11-05 18:27:14 +0900

    Add context 'date' in view.py

--- keijiban/views.py
+++ keijiban/views.py
@@ -1,4 +1,10 @@
 from django.views.generic import TemplateView
+import datetime
 
 class BoardPage(TemplateView):
     template_name = "Board.html"
+
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt["date"] = datetime.date.today()
+        return ctxt
