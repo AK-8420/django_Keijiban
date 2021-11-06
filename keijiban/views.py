7534e883c23e9f2391886ea2fa109ea91eacf1cc
Akari Katsuma <katainu0103@gmail.com>
2021-11-06 13:31:09 +0900

    Use bootstrap style in Borad.html

--- keijiban/views.py
+++ keijiban/views.py
@@ -1,5 +1,17 @@
 from django.views.generic import TemplateView
-import datetime
+from django.views.generic import ListView, DetailView
+
+'''
+# ListViewは一覧を簡単に作るためのView
+class Index(ListView):
+    # 一覧するモデルを指定 -> `object_list`で取得可能
+    model = Post
+
+# DetailViewは詳細を簡単に作るためのView
+class Detail(DetailView):
+    # 詳細表示するモデルを指定 -> `object`で取得可能
+    model = Post
+    '''
 
 class BoardPage(TemplateView):
     template_name = "Board.html"
