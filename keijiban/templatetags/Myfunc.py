ab22875ceec3492e740e4cf48fe0009b001dd358
Akari Katsuma <katainu0103@gmail.com>
2021-11-07 16:03:20 +0900

    Change date style in HTML

--- keijiban/templatetags/Myfunc.py
+++ keijiban/templatetags/Myfunc.py
@@ -1,7 +1,8 @@
 from django import template
+from ..models import Category
 
 register = template.Library()
 
-@register.filter(name="sample")
-def sample(a, b):
-    return a + b
+@register.filter(name="getCategory_list")
+def getCategory_list():
+    return Category.object.all()
